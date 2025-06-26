8a42d4b11ceefc8fd81172f02e6a067c3d9eda50
mojezhasan24 <mojezhasan@outlook.com>
2025-06-26 12:02:20 +0530

    added difference between print and println fn .

--- hello.kt
+++ hello.kt
@@ -1,3 +1,5 @@
 fun main(){ // main function is a declaration that is supposed to be in the program always and is the executable body of the program
     pinrtln("Hello, World!") // to display a msg or value in o/p we use println
+    print("i am your kotlin trainer")
+    print("lets get started") // The only difference between println() and print() is that it does not insert a new line at the end of the output where as println does.
 }

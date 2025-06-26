9521def5ed7ce54daa5f5650f9764ab0c524265b
mojezhasan24 <mojezhasan@outlook.com>
2025-06-26 10:34:02 +0530

    added communicative comments

--- hello.kt
+++ hello.kt
@@ -1,3 +1,3 @@
-fun main(){
-    pinrtln("Hello, World!")
+fun main(){ // main function is a declaration that is supposed to be in the program always and is the executable body of the program
+    pinrtln("Hello, World!") // to display a msg or value in o/p we use println
 }

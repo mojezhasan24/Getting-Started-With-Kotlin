d643fb943e862edd5e458f4559c998e0bf062741
mojezhasan24 <mojezhasan@outlook.com>
2025-07-05 22:12:49 +0530

    Add initial documentation for Boolean data type in Kotlin

---
+++ boolean.kt
@@ -0,0 +1,7 @@
+/* Boolean in kotlin is a unique yet a datatype that is present in most of the programing languages and is the base of computer functioning and logic.
+A booloean value is either:
+1. true of false
+2. on of off
+3. yes or no
+but technically speaking only implementations are true of false that a variable or constant can only have one value which is amongs these only.
+  */

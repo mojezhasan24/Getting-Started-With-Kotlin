50937873cd7758edf82112b3537a2d17fe867c18
mojezhasan24 <mojezhasan@outlook.com>
2025-07-12 23:06:12 +0530

    misc changes

--- strings.kt
+++ strings.kt
@@ -50,7 +50,7 @@ fun main() {
     val firstChar = name[0] 
     println("First character of name: $firstChar")
 
-    // String substring 
+    // String substring
     val substring = name.substring(0, 2)
     println("Substring of name: $substring")
 
